8c5db2a9f2082beca9872f786740dd226e644849
AlekseyZhukov <77729001+AlekseyZhukov@users.noreply.github.com>
2022-12-27 10:48:48 +0300

    Домашка №7

--- HM6/Program.cs
+++ HM6/Program.cs
@@ -4,93 +4,107 @@
 
 // 1, -7, 567, 89, 223-> 3
 
-Console.WriteLine("Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.");
+// Console.WriteLine("Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.");
 
-Console.WriteLine("Введите 6 целых любых чисел");
-Console.Write("Введите число 1: ");
-int num1= int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите 6 целых любых чисел");
+// Console.Write("Введите число 1: ");
+// int num1= int.Parse(Console.ReadLine());
 
-Console.Write("Введите число 2: ");
-int num2=int.Parse(Console.ReadLine());
+// Console.Write("Введите число 2: ");
+// int num2=int.Parse(Console.ReadLine());
 
-Console.Write("Введите число 3: ");
-int num3=int.Parse(Console.ReadLine());
+// Console.Write("Введите число 3: ");
+// int num3=int.Parse(Console.ReadLine());
 
-Console.Write("Введите число 4: ");
-int num4=int.Parse(Console.ReadLine());
+// Console.Write("Введите число 4: ");
+// int num4=int.Parse(Console.ReadLine());
 
-Console.Write("Введите число 5: ");
-int num5=int.Parse(Console.ReadLine());
+// Console.Write("Введите число 5: ");
+// int num5=int.Parse(Console.ReadLine());
 
-Console.Write("Введите число 6: ");
-int num6=int.Parse(Console.ReadLine());
+// Console.Write("Введите число 6: ");
+// int num6=int.Parse(Console.ReadLine());
 
- int[] newArray = {num1, num2, num3, num4, num5, num6};
- PrintArray (newArray);
+//  int[] newArray = {num1, num2, num3, num4, num5, num6};
+//  PrintArray (newArray);
 
-int AmoutMoreNull (int [] arr) {
-int count = 0;
-  for (int i=0; i<=arr.Length-1; i++) {
-    if (arr[i] >0) count++;
-  }
+// int AmoutMoreNull (int [] arr) {
+// int count = 0;
+//   for (int i=0; i<=arr.Length-1; i++) {
+//     if (arr[i] >0) count++;
+//   }
     
- return count;
-};
-void PrintArray (int[] array) {
-    string str=string.Join(", ", array);
-    Console.WriteLine($"Наш сгенерирванный массив: [{str}]");
-};
-int result = AmoutMoreNull (newArray);
-Console.WriteLine($"Количество чисел больше 0: {result}");
-
+//  return count;
+// };
+// void PrintArray (int[] array) {
+//     string str=string.Join(", ", array);
+//     Console.WriteLine($"Наш сгенерирванный массив: [{str}]");
+// };
+// int result = AmoutMoreNull (newArray);
+// Console.WriteLine($"Количество чисел больше 0: {result}");
+//Вариант 2.
+//  Console.WriteLine("Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.");
+// Console.Write("Сообщите сколько чисел вы хотите ввести: ");
+// int M= int.Parse(Console.ReadLine());
+// int[] newArray = new int [M];
+// for (int i=0; i<=M-1; i++) {
+//      Console.Write("Введите число: ");
+//      int num = int.Parse(Console.ReadLine());
+// newArray[i]= num; 
+// };
+// PrintArray(newArray);
+//  void PrintArray (int[] array) {
+//     string str=string.Join(", ", array);
+//     Console.WriteLine($"Наш сгенерирванный массив: [{str}]");
+// };
 
 
 // Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 //k1 и k2 коэфициент наклона
-Console.WriteLine("Программа, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем..");
+// Console.WriteLine("Программа, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем..");
 
-Console.WriteLine("");
-Console.Write("Введите коэффициент k1: ");
-int k1= int.Parse(Console.ReadLine());
+// Console.WriteLine("");
+// Console.Write("Введите коэффициент k1: ");
+// int k1= int.Parse(Console.ReadLine());
 
-Console.Write("Введите число b1: ");
-int b1=int.Parse(Console.ReadLine());
+// Console.Write("Введите число b1: ");
+// int b1=int.Parse(Console.ReadLine());
 
-Console.Write("Введите коэффициент к2: ");
-int k2=int.Parse(Console.ReadLine());
+// Console.Write("Введите коэффициент к2: ");
+// int k2=int.Parse(Console.ReadLine());
 
-Console.Write("Введите число b2: ");
-int b2=int.Parse(Console.ReadLine());
+// Console.Write("Введите число b2: ");
+// int b2=int.Parse(Console.ReadLine());
 
-double [] Result2=InterseptionPoints(k1, b1, k2, b2);
+// double [] Result2=InterseptionPoints(k1, b1, k2, b2);
 
-double [] InterseptionPoints (int a, int b, int c, int d ) {
-    double [] pointCoordinates= new double [2];
-    double X;
-    double Y;
-    if (a==c) {
+// double [] InterseptionPoints (int a, int b, int c, int d ) {
+//     double [] pointCoordinates= new double [2];
+//     double X;
+//     double Y;
+//     if (a==c) {
 
-        Console.WriteLine("Эти прямые не пересекаются!!!");
-        pointCoordinates[0]=0;
-        pointCoordinates[1]=0;
-        return pointCoordinates;
-    } else {
-        X=(b2-b1)/(a-c);
-        Y=(a*X)+b1;
-        pointCoordinates[0]=X;
-        pointCoordinates[1]=Y;
-        return pointCoordinates;
-    }
+//         Console.WriteLine("Эти прямые не пересекаются!!!");
+//         pointCoordinates[0]=0;
+//         pointCoordinates[1]=0;
+//         return pointCoordinates;
+//     } else {
+//         X=(b2-b1)/(a-c);
+//         Y=(a*X)+b1;
+//         pointCoordinates[0]=X;
+//         pointCoordinates[1]=Y;
+//         return pointCoordinates;
+//     }
 
-}
+// }
 
-double [] ResultPoints = InterseptionPoints(k1, b1, k2, b2);
-PrintArrayPoints(ResultPoints);
-void PrintArrayPoints (double[] array) {
-    string str=string.Join(", ", array);
-    Console.WriteLine($"Координаты точки пересечения: ({str})");
-};
+// double [] ResultPoints = InterseptionPoints(k1, b1, k2, b2);
+// PrintArrayPoints(ResultPoints);
+// void PrintArrayPoints (double[] array) {
+//     string str=string.Join(", ", array);
+//     Console.WriteLine($"Координаты точки пересечения: ({str})");
+// };
 
 
---
+++ HM7/Program.cs
@@ -0,0 +1,121 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+try 
+{
+int m = readInt("Введите число m");
+int n = readInt("Введите число n");
+int [,] ourArray=create2DArrey (m, n);
+Print2DArrey (ourArray);
+}
+catch (Exception ex)
+{
+Console.WriteLine(ex.Message);
+}
+
+int readInt (string title) {
+    Console.WriteLine(title);
+    bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+    if (isParsed) {
+        return number;
+    }
+    throw new Exception("Введены неверные данные");
+}
+
+int [,] create2DArrey (int a, int b) {
+    int [,] array = new int [a,b];
+    Random random = new Random();
+    for (var i=0; i<array.GetLength(0); i++) {
+        for (var j=0; j<array.GetLength(1); j++) {
+            array [i,j] = random.Next(-100, 100);
+        }
+    }
+    return array;
+};
+
+void  Print2DArrey (int [,] array) {
+    for (var i=0; i<array.GetLength(0); i++) {
+        for (var j=0; j<array.GetLength(1); j++) {
+            Console.Write(array[i,j]+" ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+
+// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+Console.WriteLine("Программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.");
+int str = readInt("Введите число строк");
+int stolb = readInt("Введите число столбцов");
+int [,] ourArray2=create2DArrey (str, stolb);
+Print2DArrey (ourArray2);
+Console.Write("Введите номер строки: ");
+int num1= int.Parse(Console.ReadLine());
+
+Console.Write("Введите номер столбца: ");
+int num2=int.Parse(Console.ReadLine());
+int num3=elementByTwoNumber (num1, num2, ourArray2);
+Console.WriteLine($"По этим координатам расположено число {num3}");
+
+
+int elementByTwoNumber (int a, int b, int [,] array) {
+    if (a<=array.GetLength(0) && b<=array.GetLength(1)) {
+        return array[a-1,b-1];
+    }
+    throw new Exception("Такого числа в массиве нет");
+
+}
+
+// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+Console.WriteLine("Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.");
+int str1 = readInt("Введите число строк");
+int stolb1 = readInt("Введите число столбцов");
+int [,] ourArray3=create2DArrey (str1, stolb1);
+Print2DArrey (ourArray3);
+
+
+double [] result=arithmeticMeanOfColumn (ourArray3);
+PrintArray(result);
+
+
+double [] arithmeticMeanOfColumn (int [,] array) {
+
+    double [] arrayResult =  new double [array.GetLength(1)]; 
+    for  (var i=0; i<array.GetLength(0); i++) {
+        for  (var j=0; j<array.GetLength(1); j++) {
+            arrayResult[j]+=array[i,j];
+            
+
+        }
+    }  
+    for (var i=0; i<arrayResult.Length; i++) {
+        arrayResult[i]=arrayResult[i]/array.GetLength(0);
+    }    
+return arrayResult;
+}
+void PrintArray (double[] array) {
+    string str=string.Join(", ", array);
+    Console.WriteLine($"Массив состоит из средних арифметических каждого столюца: [{str}]");
+ };
